19ead3cc8c6bd86166c981e85e85b53c9c22b5c1
Emily-GL-2025 <emilyglhk@gmail.com>
2026-01-31 12:57:56 +0800

    Delete build directory

--- build/set-desktop-toolchain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm CMakeCache.txt
-rm -r CMakeFiles/
-
-cmake . -DGAMEBOY_ADVANCE=OFF
--- build/set-gameboy-advance-toolchain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm CMakeCache.txt
-rm -r CMakeFiles/
-
-cmake -DGBA_AUTOBUILD_IMG=ON -DGBA_AUTOBUILD_CONF=ON -DCMAKE_TOOLCHAIN_FILE=$(pwd)/devkitarm.cmake .
