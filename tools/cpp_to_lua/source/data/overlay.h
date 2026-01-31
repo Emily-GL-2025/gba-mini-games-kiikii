6fa3de4a1af12fd9c5065f8f9f47ebcbc6acc09f
Emily-GL-2025 <emilyglhk@gmail.com>
2026-01-31 13:04:27 +0800

    Add files via upload

---
+++ tools/cpp_to_lua/source/data/overlay.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(overlay)
+
+//======================================================================
+//
+//	overlay, 664x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 83 tiles not compressed
+//	Total size: 512 + 2656 = 3168
+//
+//	Time-stamp: 2020-10-28, 21:59:46
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OVERLAY_H
+#define GRIT_OVERLAY_H
+
+#define overlayTilesLen 2656
+extern const unsigned int overlayTiles[664];
+
+#define overlayPalLen 512
+extern const unsigned short overlayPal[256];
+
+#endif // GRIT_OVERLAY_H
+
+//}}BLOCK(overlay)
